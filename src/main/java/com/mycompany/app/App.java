973ef288b97014b59323d675ec700bfa616507f2
jcarrascogarcia <115561940+jcarrascogarcia@users.noreply.github.com>
2022-10-19 11:19:23 -0300

    fix: agrega métodos faltantes (escaleno y pitagórico

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,21 +5,66 @@ package com.mycompany.app;
  *
  */
 public class App {
-  public static String triangulo(int a, int b, int c) {
+  public static String triangulo(int a, int b, int c) 
+  {
 
-    if (!(a + b > c && a + c > b && b + c > a)) {
+    if (!(a + b > c && a + c > b && b + c > a)) 
+    {
       return "error no es un triangulo";
     }
-    if (a == b && a == c && b == c) {
+    if (a == b && a == c && b == c) 
+    {
       return "es un triangulo equilatero";
     }
-    if (a == b || a == c || b == c) {
+    
+    if (a == b || a == c || b == c) 
+    {
       return "es un triangulo isoceles";
     }
+    
+    if(a != b && b != c && a != c)
+    { 
+      return "es un triangolo escaleno"; 
+    }
+    
+    if(esTrianguloPitagorico( a, b,  c))
+    {
+      return "es un triangolo pitagorico";
+    }
+    
     return "error no es un triangulo";
   }
+  
+  private static boolean esTrianguloPitagorico(int a,int b, int c)
+  {
+   //a2 + b2 = c2
+    if (a > b && a > c) 
+    {
+      if ((Math.pow(a,2)) == (Math.pow(b,2)) + (Math.pow(c,2)))
+      {
+        return true;
+      } 
+    } 
+    else if (b > a  &&  b > c) 
+    {
+      if ((Math.pow(b,2)) == (Math.pow(a,2)) + (Math.pow(c,2)))
+      {
+        return  true;
+      } 
+    }
+    else if (c > a  &&  c > b)
+    {
+      if ((Math.pow(c,2)) == (Math.pow(a,2)) + (Math.pow(b,2))) 
+      {
+        return  true;
+      } 
+	 } 
+    
+    return false;
+  }
 
-  public static void main(String[] args) {
+  public static void main(String[] args)
+  {
     System.out.println("Hello World!");
   }
 }
